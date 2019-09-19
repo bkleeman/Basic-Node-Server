3b9380de4bbd4d24059e84076af41c0a8a36f9a5
bkleeman <benjamin.kleeman@gmail.com>
2019-09-19 17:03:22 -0500

    remove comments, update readme

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-// var port = 3000;
-var port = process.env.PORT || 80
-// var host = "0.0.0.0";
+var port = process.env.PORT || 80;
 
 app.listen(port, function () {
   console.log('Listening on port ' + port)
